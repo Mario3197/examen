aa57e5f8dce741e94c3bbd03ad839449ad07ba56
Mario3197 <mariomoral-es@hotmail.com>
2020-03-07 13:30:19 -0600

    Cambio en environments y url web agregada

--- src/app/services/datos-examen.service.ts
+++ src/app/services/datos-examen.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Examen } from '../modelos/Examen';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,6 @@ export class DatosExamenService {
   constructor(private http: HttpClient) { }
 
   getDatos (): Observable<Examen[]> {
-    return this.http.get<Examen[]>('http://localhost:8090/mark43-service/v1/examen2');
+    return this.http.get<Examen[]>(environment.services + 'v1/examen2');
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  services: 'https://www.pruebas.mark-43.net/mark43-service/',
 };
 
 /*
